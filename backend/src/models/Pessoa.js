61e8bebf810507a0d7049ccaa45c95ef46dba85f
eduarda.oliveira <eduarda.oliveira@DESKTOP-EPQS2AC>
2023-04-27 14:14:06 -0300

    ref campos

--- backend/src/models/Pessoa.js
+++ backend/src/models/Pessoa.js
@@ -6,10 +6,10 @@ const pessoaSchema = new mongoose.Schema(
         nome: {type: String, required: true},
         genero: {type: String},
         endereco: {type: String},
-        // escolaridade: {type: String},
+        profissao: {type: String}, // ao inves de escolaridade colocar profissao
         filhos: {type: Boolean},
         qtdFilhos: {type: Number},
-        // religiao: {type: Number},
+        // religiao: {type: Number}, não vai precisar
         dataNascimento: {type: Date},
         // idade: {type: Number}, fazer o calc com base na data de nascimento
         primeiraSessao: {type: Boolean}
