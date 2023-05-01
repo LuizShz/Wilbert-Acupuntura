3a5f180e440629a13ecea7acf5374a7962ae70ba
perkles <christian.fdacr@gmail.com>
2023-05-01 12:16:57 -0300

    Main crud

---
+++ backend/src/routes/pessoaRoutes.js
@@ -0,0 +1,14 @@
+// ROTAS QUE O FRONT VAI CHAMAR
+import express from 'express';
+import PessoaController from '../controllers/pessoasController.js';
+
+const router = express.Router();
+
+router
+    .get("/pessoas", PessoaController.listarPessoas)
+    .get("/pessoas/:id", PessoaController.listarPessoaId)
+    .post("/pessoas", PessoaController.cadastrarPessoa)
+    .put("/pessoas/:id", PessoaController.atualizarPessoa)
+    .delete("/pessoas/:id", PessoaController.excluirPessoa)
+
+export default router;
